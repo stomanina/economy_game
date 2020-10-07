d06cb4b38b7e6967c579e41c53536cb8784e8f11
stomanina <64603476+stomanina@users.noreply.github.com>
2020-10-07 18:07:51 +0300

    new_js_file

---
+++ new_scripts.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function{
+
+    let container = document.createElement('div');
+        container.classList.add('container');
+        document.body.appendChild(container);
+
+        let categoryes = document.createElement('div');
+        categoryes.classList.add('categoryes');
+        document.querySelector('.container').appendChild(categoryes);
+
+        function buttonS() {
+            let button = document.createElement('button');
+                document.querySelector('.categoryes').appendChild(button);
+
+                button.classList.add('button');
+                button
+        
+            
+        }
+
+
+
+
+
+
+})
